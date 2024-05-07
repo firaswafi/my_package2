089f7cb2397c3af625fd4df7b84dca00b206d91c
firaselwafi <firaselwafi.meca.eng@gmail.com>
2024-05-07 23:31:09 +0200

    first commit

---
+++ launch/talker.py
@@ -0,0 +1,10 @@
+from launch import launch_description
+from launch_ros.actions import Node
+
+def generate_launch_discription():
+    return launch_description([
+        Node(
+            package ='demo_nodes_cpp',
+            executable='talker'
+        )
+    ])
